993b15a230d2a9a5fca583104dd390b618b6465b
Davy Williams <davywilliams@MacBook-Air-171.local>
2025-06-13 14:55:44 -0700

    Update Plaid environment to Production

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ load_dotenv()
 
 PLAID_CLIENT_ID = os.getenv('PLAID_CLIENT_ID')
 PLAID_SECRET = os.getenv('PLAID_SECRET')
-PLAID_ENV = os.getenv('PLAID_ENV', 'sandbox')
+PLAID_ENV = os.getenv('PLAID_ENV', 'production')
 PLAID_PRODUCTS = os.getenv('PLAID_PRODUCTS', 'transactions').split(',')
 PLAID_COUNTRY_CODES = os.getenv('PLAID_COUNTRY_CODES', 'US').split(',')
 
