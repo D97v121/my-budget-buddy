6d5dbdd38adc0ec6bca6d551444ebfd66873424a
Davy Williams <davywilliams@MacBook-Air-171.local>
2025-09-04 14:03:48 -0500

    Use wsgi:app to satisfy Gunicorn; factory pattern

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,3 @@
 # wsgi.py
-from dotenv import load_dotenv
-from pathlib import Path
-
-# Load the project-root .env explicitly
-load_dotenv(dotenv_path=Path(__file__).with_name(".env"))
-
 from app import create_app
 app = create_app()
-
-if __name__ == "__main__":
-    app.run()
